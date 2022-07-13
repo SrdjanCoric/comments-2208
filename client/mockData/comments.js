63ccec6f2ccdeb0590b12185b30a8bc8c2c29bee
SrdjanCoric <srki.coric@gmail.com>
2022-07-13 10:45:23 +0200

    add mock data

---
+++ client/mockData/comments.js
@@ -0,0 +1,162 @@
+export default [
+  {
+    id: "4b2d74e6-7d1a-4ba3-9e95-0f52ee8ebc6e",
+    author: "Reed Fisher",
+    body: "Sint in in sunt amet.",
+    postedAt: 1550488214207,
+    replies_count: 3,
+    replies: [
+      {
+        id: "116dbd01-d5f3-4dfb-afeb-f822a9264a5e",
+        comment_id: "4b2d74e6-7d1a-4ba3-9e95-0f52ee8ebc6e",
+        author: "Kathleen Nikolaus",
+        body: "Officia suscipit sint sint impedit nemo. Labore aut et quia quasi ut. Eos voluptatibus quidem eius delectus beatae excepturi.",
+        postedAt: 1550419941546,
+      },
+    ],
+  },
+  {
+    id: "8ec290c8-6b97-497d-90d7-f6329ba931a9",
+    author: "Carmen Boyer",
+    body: "Sapiente dolores hic maiores est quia quia. Aperiam ratione quod doloribus ex et in saepe a. Ipsam vel eos nesciunt magni enim fugiat tenetur. Ut et occaecati illum sint expedita eligendi sed. Voluptate reprehenderit numquam occaecati voluptatibus molestias. Eius officia ad eos.",
+    postedAt: 1550450855783,
+    replies_count: 5,
+    replies: [
+      {
+        id: "80ed5281-7fc7-4386-b98d-355907e6e0c1",
+        comment_id: "8ec290c8-6b97-497d-90d7-f6329ba931a9",
+        author: "Vivian Adams",
+        body: "Maxime iusto quos architecto. Voluptate consequatur porro nam tenetur voluptatem amet et esse. Natus culpa eius et sunt soluta est autem. Officia aliquid saepe. Ullam accusantium explicabo perferendis reiciendis sunt. Porro et necessitatibus.",
+        postedAt: 1550486931293,
+      },
+    ],
+  },
+  {
+    id: "cec529d4-a9c2-4095-b526-a923e730cc48",
+    author: "Buck Beier",
+    body: "Aut cumque quos veniam nulla sequi vel impedit esse.",
+    postedAt: 1550454780353,
+    replies_count: 3,
+    replies: [
+      {
+        id: "df840aac-2e85-4348-8892-b7ab31a6c603",
+        comment_id: "cec529d4-a9c2-4095-b526-a923e730cc48",
+        author: "Easter Luettgen",
+        body: "Eos maxime doloribus suscipit error.",
+        postedAt: 1550474829660,
+      },
+    ],
+  },
+  {
+    id: "32ff1d16-ef8e-4491-be3c-4a4e011ad279",
+    author: "Max Swift",
+    body: "Aut sequi quo veritatis non sit commodi.",
+    postedAt: 1550427147370,
+    replies_count: 4,
+    replies: [
+      {
+        id: "b6a43f28-fbcc-4f08-a4d7-99d3be8a77ea",
+        comment_id: "32ff1d16-ef8e-4491-be3c-4a4e011ad279",
+        author: "Janick Boyle",
+        body: "Ipsum non ipsam.",
+        postedAt: 1550445727619,
+      },
+    ],
+  },
+  {
+    id: "af3b3209-4ab1-4942-b1dc-d84260c46919",
+    author: "Shemar Konopelski",
+    body: "Placeat voluptas iure non animi nesciunt placeat qui aperiam. Nobis non molestiae saepe dolore et quam. Nihil error quod asperiores quia.",
+    postedAt: 1550487253217,
+    replies_count: 3,
+    replies: [
+      {
+        id: "d23ded68-c6b3-466a-8b3a-1cab4d4adcec",
+        comment_id: "af3b3209-4ab1-4942-b1dc-d84260c46919",
+        author: "Tierra Mitchell",
+        body: "Quisquam voluptatibus et occaecati aut non assumenda.",
+        postedAt: 1550458752656,
+      },
+    ],
+  },
+  {
+    id: "e65a1573-3c0b-4675-a07a-b61cf9244c7a",
+    author: "Matteo Spinka",
+    body: "Sint praesentium illo temporibus aut cumque illo aut voluptates.",
+    postedAt: 1550479185244,
+    replies_count: 4,
+    replies: [
+      {
+        id: "404c3aec-2dc1-44ec-8d9a-493ad3247408",
+        comment_id: "e65a1573-3c0b-4675-a07a-b61cf9244c7a",
+        author: "Filiberto Gutkowski",
+        body: "Provident consequatur veritatis aut aut quas.",
+        postedAt: 1550464504922,
+      },
+    ],
+  },
+  {
+    id: "964fca5c-9b75-4f94-8545-71853caacd2c",
+    author: "Maude Rice",
+    body: "Quia voluptates molestiae amet velit cupiditate quis suscipit quia sed. Architecto qui reprehenderit magni similique iusto. Labore deserunt aliquam. Sunt omnis eum dolor ad doloremque quasi libero eos. Suscipit omnis occaecati error et sint eos aut enim.",
+    postedAt: 1550457152343,
+    replies_count: 3,
+    replies: [
+      {
+        id: "4705f66f-7dae-4163-8396-8d250aafb9e3",
+        comment_id: "964fca5c-9b75-4f94-8545-71853caacd2c",
+        author: "Annabell Roob",
+        body: "Consequatur temporibus nemo deserunt. Sed magnam aut necessitatibus facilis. Impedit est quibusdam ut deleniti et aut modi esse culpa. Voluptatem blanditiis adipisci.",
+        postedAt: 1550447953001,
+      },
+    ],
+  },
+  {
+    id: "0538503d-db27-4786-9ace-23e762619b07",
+    author: "Abe Schuster",
+    body: "Accusamus voluptate ea est quas tempore libero amet enim culpa.",
+    postedAt: 1550446995201,
+    replies_count: 4,
+    replies: [
+      {
+        id: "05a02945-fb00-47dd-b60c-eeb4dcc81764",
+        comment_id: "0538503d-db27-4786-9ace-23e762619b07",
+        author: "Arlo Wehner",
+        body: "Aliquam voluptas facilis non quaerat mollitia omnis ab. Distinctio reiciendis repudiandae hic enim. Ab minima id illo deserunt nihil est et natus natus.",
+        postedAt: 1550431275660,
+      },
+    ],
+  },
+  {
+    id: "4c730547-7bf9-46af-a488-4e6aeabd9d05",
+    author: "Dorcas Kulas",
+    body: "Molestiae ex unde odit harum voluptatem qui dignissimos.",
+    postedAt: 1550430763932,
+    replies_count: 3,
+    replies: [
+      {
+        id: "f8178b62-df15-465f-99fd-954c39e6843f",
+        comment_id: "4c730547-7bf9-46af-a488-4e6aeabd9d05",
+        author: "Stephany Vandervort",
+        body: "Aliquid quibusdam rem.",
+        postedAt: 1550439285358,
+      },
+    ],
+  },
+  {
+    id: "f2d4a7e2-d802-47f6-b5f8-f1a1aa040cb2",
+    author: "Quincy Ziemann",
+    body: "Quis possimus in.",
+    postedAt: 1550489798249,
+    replies_count: 3,
+    replies: [
+      {
+        id: "b8784f83-2975-4526-9f0e-d55518cc8c6f",
+        comment_id: "f2d4a7e2-d802-47f6-b5f8-f1a1aa040cb2",
+        author: "Timmy Kulas",
+        body: "Rerum exercitationem ea voluptate expedita. A voluptatum inventore. Modi officiis voluptate quis molestiae iste qui dolorum ab quod. Cumque tempore dolores temporibus natus reprehenderit. Ut rem quis. Omnis eligendi aut aut.",
+        postedAt: 1550446199850,
+      },
+    ],
+  },
+];
