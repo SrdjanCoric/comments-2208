cdbefbd1d4ebb671caa30a018390ad46ce38a89e
SrdjanCoric <srki.coric@gmail.com>
2022-09-19 22:03:42 +0200

    jsx start

--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,131 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Comment = ({ author, body, postedAt, children }) => {
-  return React.createElement("div", { className: "comment" }, [
-    React.createElement("hr"),
-    React.createElement("img", {
-      src: "https://i.postimg.cc/Y0RcrdHp/no-user-image.gif",
-    }),
-    React.createElement("div", { className: "header" }, [
-      React.createElement("h3", { className: "author" }, author),
-      React.createElement("span", null, postedAt),
-    ]),
-    React.createElement("p", null, body),
-    children,
-  ]);
-};
+const App = () => (
+  <div>
+    <div className="comments">
+      <h2>Comments (2)</h2>
+      <div className="parent-comment">
+        <div className="comment">
+          <hr />
+          <div className="image">
+            <img src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif" alt="" />
+          </div>
+          <div className="header">
+            <h3 className="author">Louisa Leuschke</h3>
+            <span>2 hours ago</span>
+          </div>
+          <p>Odit eligendi quasi deleniti ipsa cumque iusto ullam.</p>
+        </div>
+        <div className="replies">
+          <div className="comment">
+            <hr />
+            <div className="image">
+              <img
+                src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif"
+                alt=""
+              />
+            </div>
+            <div className="header">
+              <h3 className="author">Cydney Robel</h3>
+              <span>2 hours ago</span>
+            </div>
+            <p>
+              Ratione quibusdam sed doloremque expedita fugit similique et aut.
+            </p>
+          </div>
+          <a href="#" className="show_more">
+            Show More Replies (2)
+          </a>
+        </div>
+      </div>
+      <div className="parent-comment">
+        <div className="comment">
+          <hr />
+          <div className="image">
+            <img src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif" alt="" />
+          </div>
+          <div className="header">
+            <h3 className="author">Rahul Fisher</h3>
+            <span>1 hour ago</span>
+          </div>
+          <p>
+            Et esse possimus temporibus. Repudiandae omnis error ab magnam
+            repudiandae maxime aspernatur consequatur. Possimus qui id non esse
+            illo ad. Temporibus sit blanditiis recusandae sunt rerum eos
+            voluptatem ducimus ullam. Cupiditate asperiores facere magnam
+            explicabo voluptatem dolores et. Numquam ex aspernatur assumenda
+            nihil sequi optio quis.
+          </p>
+        </div>
+        <div className="replies">
+          <div className="comment">
+            <hr />
+            <div className="image">
+              <img
+                src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif"
+                alt=""
+              />
+            </div>
+            <div className="header">
+              <h3 className="author">Bernice Kautzer</h3>
+              <span>2 hours ago</span>
+            </div>
+            <p>
+              Ratione quibusdam sed doloremque expedita fugit similique et aut.
+            </p>
+          </div>
+          <div className="comment">
+            <hr />
+            <div className="image">
+              <img
+                src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif"
+                alt=""
+              />
+            </div>
+            <div className="header">
+              <h3 className="author">Bernice Kautzer</h3>
+              <span>2 hours ago</span>
+            </div>
+            <p>
+              Ratione quibusdam sed doloremque expedita fugit similique et aut.
+            </p>
+          </div>
+          <div className="comment">
+            <hr />
+            <div className="image">
+              <img
+                src="https://i.postimg.cc/Y0RcrdHp/no-user-image.gif"
+                alt=""
+              />
+            </div>
+            <div className="header">
+              <h3 className="author">Bernice Kautzer</h3>
+              <span>2 hours ago</span>
+            </div>
+            <p>
+              Ratione quibusdam sed doloremque expedita fugit similique et aut.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
 
-const App = () => {
-  return React.createElement("div", { className: "comments" }, [
-    React.createElement("h2", null, "Comments(2)"),
-    React.createElement("div", { className: "parent-comment" }, [
-      React.createElement(
-        Comment,
-        { author: "Srdjan", postedAt: "2minutes", body: "Kind a boring now" },
-        React.createElement("h1", null, "Pretty interesting")
-      ),
-      React.createElement(Comment, {
-        author: "Max",
-        postedAt: "1 minute ago",
-        body: "Handling a chat",
-      }),
-      React.createElement(Comment, {
-        author: "Rodney",
-        postedAt: "2 minutes ago",
-        body: "Srdjan is the best",
-      }),
-    ]),
-  ]);
-};
+    <form action="">
+      <h2>Post a Comment</h2>
+      <div className="input-group">
+        <label>Your Name</label>
+        <input type="text" name="author" />
+      </div>
+
+      <div className="input-group">
+        <label>Your Comment</label>
+        <textarea name="body" cols="30" rows="10"></textarea>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+);
 
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(App());
